e74cb307dfafc4421ef40ea91ddda53fa05ef90c
sacunao <sheila.aobregon@gmail.com>
2016-09-14 13:46:01 -0500

    cambiando a idioma español y modificacion de contenedor del tamaño del formulario

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//Transformando en idioma español
+
+document.getElementById("form-signin-heading").innerHTML = "Por favor iniciar sesión";
+document.getElementById("inputEmail").placeholder = "Correo Electrónico";
+document.getElementById("inputPassword").placeholder = "Contraseña";
+document.querySelector("span").innerHTML  = "Recordar datos";
+document.querySelector("button").innerHTML  = "Iniciar sesión";
